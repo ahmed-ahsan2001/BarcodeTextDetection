26cb9a4bd5a04ea607508bcb05359813a712756c
ahmed-ahsan2001 <ahmed-ahsan70@hotmail.com>
2023-07-26 14:26:00 +0500

    Initial Commit

---
+++ BarcodeTextDetection/BarcodeTextDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeTextDetectionApp.swift
+//  BarcodeTextDetection
+//
+//  Created by Muhammad Ahmed on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeTextDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
